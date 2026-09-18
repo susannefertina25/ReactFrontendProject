2f6c323eb16a4e158721bf802bbe68c06babfdeb
susan <146160130+susannefertina25@users.noreply.github.com>
2026-09-18 12:01:03 +0530

    Update API endpoint for fetching todos

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     const [todos, setTodos] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:8000/api/todos/')
+        axios.get('https://backenddjangoproject.onrender.com/api/todos')
             .then(res => setTodos(res.data));
     }, []);
 
